57df0644eeaa4d193f7d14b30dfd5ba8c01d55a8
abhijeet3d2y <abhijeet3d2y@gmail.com>
2023-11-26 00:15:40 +0530

    m

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const formData = require('express-form-data');
+const morgan = require('morgan');
+
+require('colors');
+require('dotenv').config();
+
+const userRoutes = require('./routes/userRoutes.js');
+const newsRoutes = require('./routes/newsRoute.js');
+const categoryRoutes = require('./routes/categoryRoute');
+
+connectDB();
+const app = express();
+app.use(formData.parse());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/users', userRoutes);
+app.use('/api/news', newsRoutes);
+app.use('/api/category', categoryRoutes);
+
+app.get('*', function (req, res) {
+  res.status(404).json({
+    msg: "Api path not found."
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.red,
+  ),
+);
+
+
+
+// hosted server https://news-app-native.herokuapp.com/
---
+++ utils/mailer.js
@@ -0,0 +1,33 @@
+var _ = require('lodash');	
+var nodemailer = require('nodemailer');
+
+var config = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'abhiasce@gmail.com',
+        pass: 'bybybybybyby'
+    }
+};
+    
+var transporter = nodemailer.createTransport(config);
+
+var defaultMail = {
+    from: 'abhiasce@gmail.com',
+    text: 'testing maildev',
+};
+
+
+const send = (to, subject, html) => {
+    // use default setting
+    mail = _.merge({html}, defaultMail, to);
+    
+    // send email
+    transporter.sendMail(mail, function(error, info){
+        if(error) return console.log(error);
+        console.log('mail sent:', info.response);
+    });
+}
+module.exports = {
+    send
+}
